edb118cb733e5192c1529509ad3dca50f866b36d
Makoto Sano <Makoto.Sano@Mack-the-Psych.com>
2020-06-02 21:54:05 +0900

    Add test case

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM continuumio/anaconda3:4.4.0
+
+RUN pip install --upgrade pip && \
+    pip install janome
+
+RUN python -m nltk.downloader book
+
+WORKDIR /workdir
+RUN git clone https://github.com/mack-the-psych/plimac3.git
+
+RUN echo "/workdir/plimac3/Lib" > /opt/conda/lib/python3.6/site-packages/plimac-custom.pth
+RUN echo "/workdir/plimac3/Tools" >> /opt/conda/lib/python3.6/site-packages/plimac-custom.pth
+
+WORKDIR /workdir/plimac3/Resource/OANC
+RUN python compile_shelve.py
+
+WORKDIR /workdir/plimac3/Resource/BCCWJ_frequencylist_suw_ver1_0
+RUN python compile_shelve.py
---
+++ Test/En/run_01-replace_where_for_ac_text_parser-question-a-f.py
@@ -0,0 +1,16 @@
+import filecmp
+
+from replace_where_for_ac_text_parser import *
+
+data_dir = r'../../Data/En/'
+orig_dir = r'./orig_data/'
+
+words_replace_from = ['Quest', 'Stm']
+words_replace_to = ['Question', 'Stem']
+positions_where = [0, 0]
+test_file = r'Question-NAEP-G8-Reading-92-13.txt'
+
+replace_where_for_ac_text_parser(data_dir + r'Original-Question-NAEP-G8-Reading-92-13.txt', 
+                        data_dir + test_file, words_replace_from, words_replace_to, positions_where)
+
+print(test_file + r': ' + str(filecmp.cmp(data_dir + test_file, orig_dir + test_file)))
